d3c3d98090422fcbf75c2a514aa883671c40182f
Yuzzyuk <yuzzy.contact@gmail.com>
2025-07-26 20:17:25 +0200

    Create MainComponent.jsx

---
+++ components/MainComponent.jsx
@@ -0,0 +1,12 @@
+// components/MainComponent.jsx
+"use client";
+import React from "react";
+
+export default function MainComponent() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to NutriPup 🐶</h1>
+      <p>This is your dog's personalized nutrition dashboard!</p>
+    </div>
+  );
+}
