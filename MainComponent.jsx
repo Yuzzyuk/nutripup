f2c3a6da9bdb81f8251ec013d192be94f26c82e2
Yuzzyuk <yuzzy.contact@gmail.com>
2025-07-26 20:10:14 +0200

    Delete MainComponent.jsx

--- MainComponent.jsx
+++
@@ -1,9 +0,0 @@
-// components/MainComponent.jsx
-export default function MainComponent() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to NutriPup 🐶</h1>
-      <p>This is your dog's personalized nutrition dashboard!</p>
-    </div>
-  );
-}
